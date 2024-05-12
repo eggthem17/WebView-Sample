b0ea4762db81bc566c49c0a49bee27d06acc77ec
eggthem17 <paxmiler@gmail.com>
2024-05-12 14:05:10 +0900

    Initial Commit

---
+++ WebView Sample/WebView_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebView_SampleApp.swift
+//  WebView Sample
+//
+//  Created by MinKyu CHA on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WebView_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
